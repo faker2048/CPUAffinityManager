bbc4e128b8ee0c422e16360bd092a070c7c24706
fiko <nspyia2002@gmail.com>
2025-02-03 18:47:11 +0800

    feat: update Debouncer

--- Utils/Debouncer.cs
+++ Utils/Debouncer.cs
@@ -4,25 +4,47 @@ public class Debouncer : IDisposable
 {
     private readonly Action _action;
     private readonly System.Timers.Timer _timer;
+    private readonly object _lock = new();
     private bool _isWaiting;
+    private DateTime _lastCallTime = DateTime.MinValue;
 
     public Debouncer(Action action, int milliseconds)
     {
-        _action = action;
+        _action = action ?? throw new ArgumentNullException(nameof(action));
         _timer = new System.Timers.Timer(milliseconds) { AutoReset = false };
         _timer.Elapsed += (_, _) =>
         {
-            _isWaiting = false;
-            _action();
+            lock (_lock)
+            {
+                _isWaiting = false;
+                ExecuteAction();
+            }
         };
     }
 
     public void Debounce()
     {
-        if (_isWaiting) return;
+        lock (_lock)
+        {
+            if (_isWaiting) return;
 
-        _isWaiting = true;
-        _timer.Start();
+            var now = DateTime.Now;
+
+            if (now - _lastCallTime > TimeSpan.FromMilliseconds(5000))
+            {
+                ExecuteAction();
+                return;
+            }
+
+            _isWaiting = true;
+            _timer.Start();
+        }
+    }
+
+    private void ExecuteAction()
+    {
+        _action();
+        _lastCallTime = DateTime.Now;
     }
 
     public void Dispose()
